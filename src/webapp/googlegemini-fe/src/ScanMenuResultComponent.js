71450c48b12e3a987a692dc557e0db1ad3921f1b
Khang Vu <khangvu@microsoft.com>
2024-04-18 16:59:09 -0400

    Fix tests and address comments

--- src/webapp/googlegemini-fe/src/ScanMenuResultComponent.js
+++ src/webapp/googlegemini-fe/src/ScanMenuResultComponent.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import './fileupload.css';
-import {MDBCard, MDBCardBody, MDBCardImage, MDBCardText, MDBCardTitle} from "mdb-react-ui-kit";
+import {MDBBtn, MDBCard, MDBCardBody, MDBCardImage, MDBCardText, MDBCardTitle} from "mdb-react-ui-kit";
 
 
 class SnapMenuResultComponent extends Component {
@@ -11,23 +11,48 @@ class SnapMenuResultComponent extends Component {
     componentDidMount() {
     }
 
+    getCategories(items) {
+        let categories = [];
+        if (items && items.length > 0) {
+            for (const categoriesKey in items) {
+
+            }
+        }
+
+        return categories;
+    }
+
     render() {
         const result = this.props.result;
 
         return (
             <div>
-                {result.data && result.data.constructor === Array && result.data.map((item) =>
-                    <MDBCard className='m-3'>
-                        <MDBCardImage style={{maxHeight: '30vh', objectFit: 'cover'}}
-                                      src={item.imageUrls[0]} position='top' alt={item.name} />
-                        <MDBCardBody>
-                            <MDBCardTitle>{item.name}</MDBCardTitle>
-                            <MDBCardText>
-                                {item.description}
-                            </MDBCardText>
-                        </MDBCardBody>
-                    </MDBCard>
-                )}
+                {result.data && result.data.constructor === Array && result.data.length > 0 &&
+                    <div className="pt-3">
+                        <div>
+                            <MDBBtn rounded className='mx-2' color='dark'>
+                                Dark
+                            </MDBBtn>
+                            <MDBBtn rounded className='text-grey' color='light'>
+                                Light
+                            </MDBBtn>
+                        </div>
+
+                        {result.data.map((item) =>
+                        <MDBCard className='m-3'>
+                            <MDBCardImage style={{maxHeight: '30vh', objectFit: 'cover'}}
+                                          src={item.imageUrls && item.imageUrls.length > 0 ? item.imageUrls[0] : ""}
+                                          position='top'
+                                          alt={item.name}/>
+                            <MDBCardBody>
+                                <MDBCardTitle>{item.name}</MDBCardTitle>
+                                <MDBCardText>
+                                    {item.description}
+                                </MDBCardText>
+                            </MDBCardBody>
+                        </MDBCard>)}
+                    </div>
+                }
                 {result.error && <div>{result.error}</div>}
             </div>
         )
