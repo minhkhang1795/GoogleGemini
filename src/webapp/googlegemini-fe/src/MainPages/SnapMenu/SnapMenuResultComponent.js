ef1a967776a931053d628bcc9b4f7db3588f8ffa
Khang Vu <khangvu@microsoft.com>
2024-04-26 15:25:43 -0400

    Add item details

--- src/webapp/googlegemini-fe/src/MainPages/SnapMenu/SnapMenuResultComponent.js
+++ src/webapp/googlegemini-fe/src/MainPages/SnapMenu/SnapMenuResultComponent.js
@@ -1,13 +1,24 @@
 import React, {Component} from 'react';
 import '../../fileupload.css';
-import {MDBBtn, MDBCard, MDBCardBody, MDBCardImage, MDBCardText, MDBCardTitle} from "mdb-react-ui-kit";
+import {
+    MDBBtn,
+    MDBCard,
+    MDBCardBody,
+    MDBCardImage,
+    MDBCardText,
+    MDBCardTitle,
+    MDBModal, MDBModalBody, MDBModalContent,
+    MDBModalDialog, MDBModalFooter, MDBModalHeader, MDBModalTitle
+} from "mdb-react-ui-kit";
 import LoadingComponent from "../../Utils/LoadingComponent";
 
 
 class SnapMenuResultComponent extends Component {
     state = {
         userProfile: "love Japanese foods, lactose intolerance, gluten free, love beef and meats",
-        currentCategory: "All"
+        currentCategory: "All",
+        showModal: false,
+        currentItem: null,
     };
 
     componentDidMount() {
@@ -39,6 +50,14 @@ class SnapMenuResultComponent extends Component {
         return categories;
     }
 
+    popUpItemDetails(item) {
+        this.setState({showModal: true, currentItem: item});
+    }
+
+    toggleModal(show) {
+        this.setState({showModal: show});
+    }
+
     render() {
         const result = this.props.result;
         const isLoading = this.props.isLoading;
@@ -64,7 +83,8 @@ class SnapMenuResultComponent extends Component {
 
                         <div>
                             {this.getItemsByCategory(result.data).map((item) =>
-                                <MDBCard className='m-3' key={item.name}>
+                                <MDBCard className='m-3' key={item.name}
+                                         onClick={() => this.popUpItemDetails(item)}>
                                     <MDBCardImage style={{maxHeight: '30vh', objectFit: 'cover'}}
                                                   src={item.imageUrls && item.imageUrls.length > 0 ? item.imageUrls[0] : ""}
                                                   position='top'
@@ -83,6 +103,35 @@ class SnapMenuResultComponent extends Component {
                 {isLoading && <LoadingComponent className="text-center"
                                                 style={{marginTop: '50%'}}
                                                 loadingMessage='Our chef is preparing your menu!'/>}
+
+                {/* Popup modal to show item detail */}
+                <MDBModal tabIndex='-1' open={this.state.showModal} onClose={() => this.toggleModal()}>
+                    <MDBModalDialog centered scrollable>
+                        <MDBModalContent>
+                            <MDBCardImage style={{maxHeight: '30vh', objectFit: 'cover'}}
+                                          src={this.state.currentItem?.imageUrls && this.state.currentItem?.imageUrls.length > 0 ? this.state.currentItem?.imageUrls[0] : ""}
+                                          position='top'
+                                          alt={this.state.currentItem?.name}/>
+                            {/*<MDBBtn className='btn-close' color='none' onClick={() => this.toggleModal()}></MDBBtn>*/}
+                            <MDBModalBody>
+                                <h3 className="text-dark">{this.state.currentItem?.name}</h3>
+                                <p>
+                                    {this.state.currentItem?.description}
+                                    <br/>
+                                    <br/>
+                                    <b className="text-dark">Why we recommend this?</b>
+                                    <br/>
+                                    {this.state.currentItem?.matchExplanation}
+                                </p>
+                            </MDBModalBody>
+                            <MDBModalFooter>
+                                <MDBBtn rounded color='dark' onClick={() => this.toggleModal()}>
+                                    Close
+                                </MDBBtn>
+                            </MDBModalFooter>
+                        </MDBModalContent>
+                    </MDBModalDialog>
+                </MDBModal>
             </div>
         )
     }
